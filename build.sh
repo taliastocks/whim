be70bca2ea2274d59931192415fb94353f8a8947
Collin RM Stocks <collinstocks@gmail.com>
2019-10-07 22:09:51 -0400

    use python3 instead of specific python version in pex shebang

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 -m pex --disable-cache -v . --python=python3 -m whim -o whim
+python3 -m pex --disable-cache -v . --python=python3 --python-shebang='/usr/bin/env python3' -m whim -o whim
